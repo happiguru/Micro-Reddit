6148e80215ed2856bb20854239e9cbff341aed77
solbencas <cecibenitezca@gmail.com>
2020-09-29 15:23:08 -0400

    Add user profile model and migrations.

--- test.rb
+++ test.rb
@@ -28,6 +28,82 @@ class Courses < ApplicationRecord
   has_many :lessons
 end
 
+# profile Model
+class UserProfile < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :age
+  validates :gender, presence: true
+  add_reference :user_profile, :city, index: true 
+  add_foreign_key :user_profile, :city
+  add_reference :user_profile, :state, index: true 
+  add_foreign_key :user_profile, :state
+  add_reference :user_profile, :country, index: true 
+  add_foreign_key :user_profile, :country
+  has_many :city
+  has_many :state 
+  has_one :country
+end
+
+class City < ApplicationRecord
+  validates :city_name, presence: true
+  belongs_to :state 
+  belons_to :country
+end
+
+class State < ApplicationRecord
+  validates :state_name, presence: true
+  belongs_to :country
+  has_many :city
+end
+
+class Country < ApplicationRecord
+  validates :country_name, presence: true, uniqueness: true
+  has_many :state
+  has_many :city
+end
+# profile Migration 
+class CreateUserProfile < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_profile do |t|
+      t.string :username
+      t.string :email
+      t.int :age
+      t.string :gender 
+      t.references :city, foreign_key: true
+      t.references :state, foreign_key: true
+      t.references :country, foreign_key: true
+      t.timestamps
+    end
+  end
+end
+
+class CreateCity < ActiveRecord::Migration[5.2]
+  def change
+    create_table :city do |t|
+      t.string :city_name
+      t.timestamps
+    end
+  end
+end
+
+class CreateState < ActiveRecord::Migration[5.2]
+  def change
+    create_table :state do |t|
+      t.string :state_name
+      t.timestamps
+    end
+  end
+end
+
+class CreateCountry < ActiveRecord::Migration[5.2]
+  def change
+    create_table :country do |t|
+      t.string :country_name
+      t.timestamps
+    end
+  end
+end
 # This is the migration code for the models.
 # micro_reddit Migration
 class CreateAuthors < ActiveRecord::Migration[5.2]
