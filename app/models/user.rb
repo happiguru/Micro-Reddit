e3a0b0a6208be775fbad453be2d153ed88a46203
hguruman <hguruman@gmail.com>
2020-10-01 22:05:03 +0400

    Update validation for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true 
+  validates :username, presence: true, uniqueness: true, length: ( 8..16 )
 
   has_many :posts
   has_many :comments
